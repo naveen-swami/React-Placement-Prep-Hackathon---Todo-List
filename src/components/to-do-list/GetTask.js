9890947171bf38a7901316f2b682842bd87a5ddc
naveen-swami <naveen.swami@vidu.tech>
2021-06-30 22:39:43 +0530

    fixed remove issue

--- src/components/to-do-list/GetTask.js
+++ src/components/to-do-list/GetTask.js
@@ -1,9 +1,13 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 export default function GetTask({ index, item, removeTask, editTask }) {
     console.log("In side get task",index, item);
     const [isEdit, setIsEdit] = useState(false);
     const [task, setTask] = useState(item);
+
+    useEffect(() => {
+        setTask(item);
+    }, [item]);
 
     function editHandler() {
         console.log(index);
@@ -19,18 +23,18 @@ export default function GetTask({ index, item, removeTask, editTask }) {
 
     return (
         <>
-            <li className="list"> {index} || {task}
+            {/* <li className="list"> {index} || {task}
                 <button style={{ marginLeft: 5 }} className="edit" onClick={() => editHandler()}> Edit</button>
                 <button style={{ marginRight: 3 }} className="delete" onClick={() => removeTask(index)}> Delete</button>
-            </li>
-            {/* {!isEdit ? (<li className="list"> {index} || {task}
+            </li> */}
+            {!isEdit ? (<li className="list"> {index} || {task}
                 <button style={{ marginLeft: 5 }} className="edit" onClick={() => editHandler()}> Edit</button>
                 <button style={{ marginRight: 3 }} className="delete" onClick={() => removeTask(index)}> Delete</button>
             </li>) : (
                 <div >
                     <textarea style={{ marginRight: 5 }} value={task} className="editTask" onChange={() => setTask(event.target.value)}></textarea>
                     <button className="saveTask" onClick={() => saveTask()}> Save Task </button>
-                </div>)} */}
+                </div>)}
         </>
     )
 }
